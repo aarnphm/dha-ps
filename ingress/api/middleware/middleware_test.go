407e7d65c1aaebf3d457910c10f80e21b105953a
aar0npham <aaronpham0103@gmail.com>
2020-09-03 13:59:01 -0400

    updated circle configs with WIP middleware test

--- ingress/api/middleware/middleware_test.go
+++ ingress/api/middleware/middleware_test.go
@@ -3,34 +3,64 @@ package middleware_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"sync"
+	"sync/atomic"
 	"testing"
 
 	"github.com/aarnphm/dha-ps/ingress/api/middleware"
-	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRateLimiter(t *testing.T) {
+	is := require.New(t)
 
-	handler := middleware.RateLimiter(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`test`))
-	}))
-	for i := 1; i < 20; i++ {
-		req, err := http.NewRequest("GET", "/", nil)
-		if err != nil {
-			log.Fatalf("Error: [%+v]", err)
+	req, err := http.NewRequest("GET", "/", nil)
+	is.NoError(err)
+	is.NotNil(req)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, thr := w.Write([]byte(`test`))
+		if thr != nil {
+			panic(thr)
 		}
-		rr := httptest.NewRecorder()
-		handler.ServeHTTP(rr, req)
-		ch := make(chan int)
-		go func() {
-			rr := httptest.NewRecorder()
-			handler.ServeHTTP(rr, req)
-			// hmm should be limited but not
-			if status := rr.Code; status != http.StatusTooManyRequests {
-				t.Errorf("handler returned wrong status code, got %v want %v", status, http.StatusTooManyRequests)
-			}
-			close(ch)
-		}()
-		<-ch
+	})
+
+	mddlw := middleware.RateLimiter(handler)
+	is.NotZero(mddlw)
+
+	success := int64(3)
+	clients := int64(30)
+
+	// sequential
+	for i := int64(1); i <= clients; i++ {
+		resp := httptest.NewRecorder()
+		mddlw.ServeHTTP(resp, req)
+		if i <= success {
+			is.Equal(resp.Code, http.StatusOK)
+		} else {
+			is.Equal(resp.Code, http.StatusTooManyRequests)
+		}
 	}
+
+	// concurrent
+	mddlw = middleware.RateLimiter(handler)
+	is.NotZero(mddlw)
+	wg := &sync.WaitGroup{}
+
+	counter := int64(0)
+	for i := int64(1); i <= clients; i++ {
+		wg.Add(1)
+		go func() {
+
+			rr := httptest.NewRecorder()
+			mddlw.ServeHTTP(rr, req)
+			// hmm should be limited but not
+			if status := rr.Code; status == http.StatusOK {
+				atomic.AddInt64(&counter, 1)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	is.Equal(success, atomic.LoadInt64(&counter))
 }
